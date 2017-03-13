4a63d2edfde485dca8ad780e4410c4875efaf443
Chiefautoparts <chiefautoparts@outlook.com>
2017-03-13 15:20:59 -0700

    think ive got the functions to work

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var pike = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
-  return: Math.floor((Math.random * (maxCust - minCust) + 1) + minCust)
-};
-var hours = Array['6am', '7am', '8am', '9am', ];
-var hourlyNum = function(hours){
-  for(var i = 0, i < 15,)
+  randomCust: function() {
+    return Math.floor((Math.random * (this.maxCust - this.minCust) + 1) + this.minCust);
+  },
+  totalSales: [],
+//var hours = ['6am ' + pike, '7am ' + pike, '8am ' + pike, '9am ' + pike, '10am ' + pike, '11am ' + pike, '12pm ' + pike, '1pm ' + pike, '2pm ' + pike, '3pm ' + pike, '4pm ' + pike, '5pm ' + pike, '6pm ' + pike, '7pm ' + pike, '8pm ' + pike];
+  hourlyNum: function() {
+    totalSales.length = 15;
+    var totalCookies = 0;
+    for(var i = 0; i < 15; i++){
+      tatalSales[i] = this.randomCust() * this.avgSale;
+      totalCookies++;
+    }
+  }
 };
