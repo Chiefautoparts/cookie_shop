4874a3f4b2e4644c541dcc522ae2dd54612971ab
Chiefautoparts <chiefautoparts@outlook.com>
2017-03-13 14:36:57 -0700

    made array for hourly cookie numbers

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ var pike = {
   avgSale: 6.3,
   return: Math.floor((Math.random * (maxCust - minCust) + 1) + minCust)
 };
+var hours = Array['6am', '7am', '8am', '9am', ];
+var hourlyNum = function(hours){
+  for(var i = 0, i < 15,)
+};
