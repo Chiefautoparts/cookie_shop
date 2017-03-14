2c467476edae76eafe1d97af5d708f6c47bd3f35
Chiefautoparts <chiefautoparts@outlook.com>
2017-03-14 13:06:41 -0700

    couple changes made to app.js nothing major though

--- app.js
+++ app.js
@@ -2,15 +2,13 @@
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'total'];
 
-var body = document.getElementsByTagName('body')[0];
-
 var firstPike = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
   salesArr: [],
   name: 'First and Pike',
-  randCust: function () {
+  randomCust: function () {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookieSold: function(){
